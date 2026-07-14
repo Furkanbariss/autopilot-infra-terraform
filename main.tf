9621291dfdcad9147ba8f26f3d8c7ec7871c65b7
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2026-07-14 10:32:20 +0300

    fixed ec2 version

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 resource "aws_security_group" "web_sg" {
   name        = "${var.project_name}-web-sg-tf"
-  description = "SSH ve HTTP erişimi için"
+  description = "SSH ve HTTP erisimi icin"
 
   ingress { # (in gress) içeri giriş portları
     description = "SSH"
@@ -64,7 +64,7 @@ data "aws_ami" "my_ubuntu" {
 
 resource "aws_instance" "web_server" {
   ami                    = data.aws_ami.my_ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = data.aws_key_pair.existing_key.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
