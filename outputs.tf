9f5534010b74f6197ba756b940adefa4d81a8200
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2026-07-29 18:07:45 +0300

    CloudWatch Alarms and SNS implemented

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-output "instance_public_ip" {
-  description = "Web sunucusunun public IP adresi"
-  value       = aws_instance.web_server.public_ip
-}
+# output "instance_public_ip" {
+#   description = "Web sunucusunun public IP adresi"
+#   value       = aws_instance.web_server.public_ip
+# }
 
 output "security_group_id" {
   description = "Olusturulan security group ID"
   value       = aws_security_group.web_sg.id
 }
 
-output "server_name" {
-  value = aws_instance.web_server.tags["Name"]
-}
+# output "server_name" {
+#   value = aws_instance.web_server.tags["Name"]
+# }
 
 output "alb_dns_name" {
   value = aws_lb.app.dns_name
--- variables.tf
+++ variables.tf
@@ -9,3 +9,12 @@ variable "project_name" {
   type        = string
   default     = "furkan-autopilot"
 }
+
+# variable "api_key_example" {
+#   description = "AWS secret manager'da kullanılmak üzere oluşturduğum örnek KEY'ler"
+#   type = map(string) # hepsine sıra sıra uygulanması için map fonksiyonunu kullandık
+#   default = {
+#     API_KEY     = "example-willChange-api-key"
+#     DB_PASSWORD = "example-willChange-password"
+#   }
+# }
