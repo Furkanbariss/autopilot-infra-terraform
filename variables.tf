05bb6faa4347796ba63dc9029e64f806573d1b68
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2026-07-30 15:12:40 +0300

    cleaning some comment code

--- variables.tf
+++ variables.tf
@@ -10,11 +10,3 @@ variable "project_name" {
   default     = "furkan-autopilot"
 }
 
-# variable "api_key_example" {
-#   description = "AWS secret manager'da kullanılmak üzere oluşturduğum örnek KEY'ler"
-#   type = map(string) # hepsine sıra sıra uygulanması için map fonksiyonunu kullandık
-#   default = {
-#     API_KEY     = "example-willChange-api-key"
-#     DB_PASSWORD = "example-willChange-password"
-#   }
-# }
